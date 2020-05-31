21f9a4cee7d23f310ab1812a32a3727fbab2b17e
Mr. B.K SINGH <42309849+mrbksingh@users.noreply.github.com>
2020-05-31 18:19:48 +0530

    Update dataset_split.py

--- dataset_split.py
+++ dataset_split.py
@@ -19,7 +19,7 @@ def load_data(validation=False, test=False):
         labels = np.load(join(SAVE_DIRECTORY, SAVE_DATASET_LABELS_FILENAME))
         labels = labels.reshape([-1, len(EMOTIONS)])
         X_train, X_test, y_train, y_test = train_test_split(images, labels, train_size=0.8, test_size=0.2, random_state=42)
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.7, test_size=0.3, random_state=42)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.9, test_size=0.1, random_state=42)
 
         train_dict['X'] = X_train
         train_dict['Y'] = y_train
